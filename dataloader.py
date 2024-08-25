14cb8cccf42a60ed2fc3525d1a8552452d26cf50
Nathan Hay <72686983+NathanBHay@users.noreply.github.com>
2024-08-25 16:21:32 +1000

    Added example data, and dataloader

---
+++ dataloader.py
@@ -0,0 +1,16 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from pydicom import dcmread
+from pydicom.data import get_testdata_file
+from pydicom.pixel_data_handlers.util import (
+    apply_color_lut,
+    apply_modality_lut,
+    apply_voi_lut,
+)
+
+x = "./stage_2_test/ID_000a2d7b0.dcm"
+y = dcmread(x)
+z = y.pixel_array
+# plt.imshow(apply_color_lut(z, y, palette="SUMMER"), cmap="gray")
+plt.show()
